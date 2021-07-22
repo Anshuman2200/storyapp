323a51966d1a792fdbcc6a6dfc54f9e59e8f56ac
Anshuman Kumar <Ansuman59@gmail.com>
2021-07-23 00:13:45 +0530

    story app

---
+++ client/src/styles.js
@@ -0,0 +1,20 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 20,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0F8FF'
+  },
+  heading: {
+    color:'#9E7BFF'
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
